3ca9b80d5316f8ef524c381655856c24f3fef7b7
Loïc Peyramaure <loic63130@gmail.comm>
2025-11-08 17:21:42 +0100

    Mise en place de la DB local / distante et de la syncho

--- Infradon2_1/src/App.vue
+++ Infradon2_1/src/App.vue
@@ -5,6 +5,7 @@ export default {
   data() {
     return {
       db: null as any,
+      remoteDB: null as any,
       documents: [] as any[],
     }
   },
@@ -14,10 +15,28 @@ export default {
   },
 
   methods: {
-    //https://pouchdb.com/api.html#create_database
     initDB() {
-      this.db = new PouchDB('http://admin:de4Dke032e!@localhost:5984/ma_db')
+      this.db = new PouchDB('ma_db_locale')
+      this.remoteDB = new PouchDB('http://admin:de4Dke032e!@localhost:5984/ma_db')
+
       this.recupererTousLesDocs()
+    },
+
+    synchroniser() {
+      this.db
+        .sync(this.remoteDB)
+        .on('change', (info: any) => {
+          console.log('Changement détecté during sync', info)
+          this.recupererTousLesDocs()
+        })
+        .on('complete', (info: any) => {
+          console.log('Synchro terminée !', info)
+          this.recupererTousLesDocs()
+          alert('Synchronisation terminée !')
+        })
+        .on('error', (err: any) => {
+          console.error('Erreur de synchro', err)
+        })
     },
 
     recupererTousLesDocs() {
@@ -34,13 +53,12 @@ export default {
     genererObjetDemo() {
       const timestamp = Date.now()
       return {
-        _id: 'le meilleur nom de l univers... oui j en ai marre' + timestamp,
-        title: 'Doc ' + timestamp,
-        content: 'cest super fake ' + timestamp,
+        _id: 'local_doc_' + timestamp,
+        title: 'Doc Local ' + timestamp,
+        content: 'Contenu créé localement ' + timestamp,
       }
     },
 
-    //https://pouchdb.com/api.html#create_document
     ajouterDoc() {
       const nouveauDoc = this.genererObjetDemo()
 
@@ -55,13 +73,11 @@ export default {
         })
     },
 
-    // https://pouchdb.com/api.html#fetch_document
     modifDoc(doc: any) {
       this.db
         .get(doc._id)
         .then((docActuel: any) => {
-          docActuel.content = docActuel.content + ' , modif le : ' + new Date().toLocaleString() // Rajouter la date de la modif pour une dose légendaire de fun YAY
-
+          docActuel.content = docActuel.content + ' (Modifié)'
           return this.db.put(docActuel)
         })
         .then((resultat: any) => {
@@ -73,7 +89,6 @@ export default {
         })
     },
 
-    // https://pouchdb.com/api.html#delete_document
     supprimerDoc(doc: any) {
       this.db
         .remove(doc._id, doc._rev)
@@ -90,16 +105,27 @@ export default {
 </script>
 
 <template>
-  <div>
-    <button @click="ajouterDoc">Ajouter un doc</button>
+  <div style="padding: 20px">
+    <div style="margin-bottom: 20px">
+      <button @click="ajouterDoc">1. Ajouter un doc (Local)</button>
+      <button @click="synchroniser" style="margin-left: 10px; background-color: lightblue">
+        2. 🔄 Synchroniser avec le serveur
+      </button>
+    </div>
 
-    <div v-for="doc in documents" :key="doc._id">
+    <h3>Documents (Base Locale)</h3>
+    <div v-if="documents.length === 0">Aucun document local.</div>
+
+    <div
+      v-for="doc in documents"
+      :key="doc._id"
+      style="border: 1px solid #ccc; margin: 10px 0; padding: 10px"
+    >
       <p><strong>ID:</strong> {{ doc._id }}</p>
       <p><strong>Titre:</strong> {{ doc.title }}</p>
       <p><strong>Contenu:</strong> {{ doc.content }}</p>
-      <button @click="modifDoc(doc)">Modif</button>
-      <button @click="supprimerDoc(doc)">Suppr</button>
-      <hr />
+      <button @click="modifDoc(doc)">Modif Local</button>
+      <button @click="supprimerDoc(doc)">Suppr Local</button>
     </div>
   </div>
 </template>
